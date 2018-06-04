b4df28b2323b53b70233027e1391c974cf29a497
Derek777 <vvcompany777@gmail.com>
2018-06-04 22:20:52 +0300

    simple gulp tasks

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var gulp         = require('gulp'), // Подключаем Gulp
-    scss         = require('gulp-sass'), //Подключаем Sass пакет,
+    sass         = require('gulp-sass'), //Подключаем Sass пакет,
     browserSync  = require('browser-sync'), // Подключаем Browser Sync
     concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
     uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
@@ -15,14 +15,11 @@ gulp.task('mytask', function() {
     console.log('Привет, я таск!');
 });
 
-gulp.task('scss', function(){ // Создаем таск Sass
-    return gulp.src('app/scss/style.scss') // Берем источник
-        .pipe(scss()) // Преобразуем Sass в CSS посредством gulp-sass
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('app/scss/**/*.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
-        .pipe(cssnano()) // Сжимаем
-        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-        .pipe(gulp.dest('app/css')) // Выгружаем в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
@@ -46,10 +43,16 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
 });
 
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('app/css/style.css') // Выбираем файл для минификации
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
+});
 
-gulp.task('watch', ['browser-sync', 'scripts', 'scss'], function() {
-    gulp.watch('app/scss/**/*.scss', ['scss']); // Наблюдение за sass файлами в папке sass
-    gulp.watch('app/css/style.css', browserSync.reload);
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('app/css/style.css', ['css-libs']);
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
